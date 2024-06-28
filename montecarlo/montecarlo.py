b99bedd66c5df52880fe3d3baf68361eee7f3230
Finn <107573572+finnmok@users.noreply.github.com>
2024-06-28 10:41:16 -0400

    Update montecarlo.py

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,9 +1,10 @@
 class Die():
     def self.__init__(self):
+        pass
         
 class Game():
     def self.__init__(self):
-        
+        pass
 class Analyzer():
     def self.__init__(self):
-        
+        pass
