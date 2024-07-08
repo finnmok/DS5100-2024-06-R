f0ccf70d4f14943d3c2b2beeecf958ac293cfb50
Finn Mokrzycki <qcp6az@virginia.edu>
2024-07-08 07:29:25 -0400

    same

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+# import pandas as pd
+setup(
+    name='montecarlo',
+    version='1.0',
+    author='Finn Mokrzycki',
+    author_email='finn.mokrzycki@virginia.edu',
+    description='Monte Carlo',
+    url='https://github.com/finnmok/DS5100-2024-06-R',
+    packages=['montecarlo'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
